f9ce97adb156bc5a8abfb48fb86abd06f9110950
henrygarciaospina <henrygarcia0309@gmail.com>
2017-08-12 18:07:18 -0500

    Video 31. implementación de comments de los blogs

--- app/controllers/public_posts_controller.rb
+++ app/controllers/public_posts_controller.rb
@@ -6,10 +6,25 @@ class PublicPostsController < ApplicationController
   end
 
   def show
+    @post = Post.find_by url: params[:url]
+
+    @comments = @post.comments
+
+    @comment = Comment.new
+  end
+
+  def create_comment
+    @comment = Comment.new name: params[:name], content: params[:content], post_id: params[:post_id]
+
+    if @comment.save
+      redirect_to :back, notice: 'The comment was successfully created'
+    end
   end
 
   def posts_category
   	@category = Category.find(params[:id])
+
   	@posts = @category.posts.published
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
   root 'public_posts#index'
 
-  get 'public_posts/show'
-
   get 'categories/:id', to: 'public_posts#posts_category', as: 'posts_category'
 
+  get 'posts/:url', to: 'public_posts#show', as: 'posts_show'
+
   devise_for :users
+
   resources :meets
+
   resources :ads
+
   resources :comments
+  post 'create_comment', to: 'public_posts#create_comment', as: 'create_comment'
+
   resources :categories
+
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
